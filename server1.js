fd22fc17f16c274b13dc6c48846ae9a3694df4ed
Kmasteller <kennethmasteller@gmail.com>
2017-12-10 13:46:07 -0700

    update 1

--- server1.js
+++ server1.js
@@ -14,10 +14,10 @@ function handleRequest2(request, response) {
 var server1 = http.createServer(handleRequest);
 var server2 = http.createServer(handleRequest2);
 
-server.listen(PORT1, function() {
+server1.listen(PORT1, function() {
     console.log("Server listening on: http://localhost:" + PORT1);
 })
 
-server.listen(PORT2, function() {
+server2.listen(PORT2, function() {
     console.log("Server listening on: http://localhost:" + PORT2);
 })
